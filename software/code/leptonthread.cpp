305d3bdfab3d2bcefd7d0394abe9ad673651a7dd
Francesco <francesco.argentieri89@gmail.com>
2020-02-11 19:35:18 +0100

    add: chunck code

--- software/code/leptonthread.cpp
+++ software/code/leptonthread.cpp
@@ -34,17 +34,12 @@ LeptonThread::~LeptonThread() {
 }
 
 void LeptonThread::run() {
-  // generate thermal initial image
   m_ir_image = QImage(80, 60, QImage::Format_RGB888);
-  // open SPI port
   leptonSPI_OpenPort(0);
   usleep(LeptonLoadTime);
   while (true) {
-    // read data packets from lepton over SPI
     int resets = 0;
     for (int j = 0; j < PACKETS_PER_FRAME; j++) {
-      // if it's a drop packet, reset j to 0, set to -1 so he'll be at 0 again
-      // loop
       read(spi_cs0_fd, result + sizeof(uint8_t) * PACKET_SIZE * j,
            sizeof(uint8_t) * PACKET_SIZE);
       int packetNumber = result[j * PACKET_SIZE + 1];
@@ -59,28 +54,18 @@ void LeptonThread::run() {
         }
       }
     }
-#if LOGGER
-    if (resets >= MaxResetsPerFrame) {
-      LOG(DEBUG, "reading, resets: %d", resets)
-    }
-#endif
-
     m_frameBuffer = (uint16_t *)result;
     int row, column;
     uint16_t value;
     uint16_t minValue = 65535;
     uint16_t maxValue = 0;
-
     for (int i = 0; i < FRAME_SIZE_UINT16; i++) {
-      // skip the first 2 uint16_t's of every packet, they're 4 header bytes
       if (i % PACKET_SIZE_UINT16 < 2) {
         continue;
       }
-      // flip the MSB and LSB at the last second
       auto temp = result[i * 2];
       result[i * 2] = result[i * 2 + 1];
       result[i * 2 + 1] = temp;
-      // find value min/max for proper scale
       value = m_frameBuffer[i];
       if (value > maxValue) {
         maxValue = value;
@@ -91,7 +76,6 @@ void LeptonThread::run() {
       column = i % PACKET_SIZE_UINT16 - 2;
       row = i / PACKET_SIZE_UINT16;
     }
-
     float diff = static_cast<float>(maxValue - minValue);
     float scale = 255 / diff;
     QRgb color;
@@ -112,7 +96,6 @@ void LeptonThread::run() {
     recalculateResult(m_ir_image.scaled(640, 480, Qt::KeepAspectRatio),
                       colour_image.scaled(640, 480, Qt::KeepAspectRatio));
   }
-  //  close SPI port
   leptonSPI_ClosePort(0);
 }
 
